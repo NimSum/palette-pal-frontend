4949d434628df38f221ea44f2ac84e4aa2c7dd87
Lynne Rang <lynne.rang@gmail.com>
2019-07-02 19:57:43 -0600

    Add subheader component.

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './_App.scss';
 import Header from '../Header';
+import SubHeader from '../SubHeader';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SubHeader />
       <main>
         <Switch>
           {/* <Route exact path="/" component={PickerScreen} /> */}
