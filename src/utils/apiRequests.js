471bafd75b2c6e3c78f9b18caa4f29cb8dd47e4b
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-05 16:50:41 -0600

    Create requests object for different fetches

--- src/utils/apiRequests.js
+++ src/utils/apiRequests.js
@@ -4,10 +4,16 @@ const urls = {
   palettes: 'https://palette-pal-be.herokuapp.com/api/v1/palettes'
 }
 
-
+const requests = {
+  getProjects: () => fetchAnything(urls.projects),
+  getPalettes: () => fetchAnything(urls.palettes),
+  getDetailedProjects: () => fetchAnything(urls.projWithPaletes)
+}
 
 export async function fetchAnything(url) {
   const response = await fetch(url);
   return response.json();
 }
 
+
+export default requests;
