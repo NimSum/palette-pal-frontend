f957051018535bb20084708bd4ea91b4ea138584
Lynne Rang <lynne.rang@gmail.com>
2019-07-05 15:09:53 -0600

    Convert pickercolor to functional component.

--- src/PickerColor/index.js
+++ src/PickerColor/index.js
@@ -1,33 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PickerColor extends Component {
-  constructor(props) {
-    super(props);
-  }
+function PickerColor(props){
+	const checkbox = props.held ? 'far fa-check-square' : 'far fa-square';
+	const holdClass = props.held ? 'hold' : null;
 
-  // toggleHold = () => {
-  //   this.props.toggleHold(this.props.id);
-  // }
-
-  render() {
-    let checkbox;
-    let holdClass;
-
-    if (this.props.held) {
-      checkbox = 'far fa-check-square';
-      holdClass = 'hold';
-    } else {
-      checkbox = 'far fa-square';
-      holdClass = null; 
-    }
-
-    return (
-      <div className="picker-color" style={{ backgroundColor: this.props.color, color: this.props.color }}>
-        <p className="picker-color-value">{this.props.color}</p>
-        <p className={`picker-hold ${holdClass}`} onClick={() => this.props.toggleHold(this.props.id)}><i className={checkbox}></i>HOLD</p>
-      </div>
-    );
-  }
+	return (
+		<div className="picker-color" style={{ backgroundColor: props.color, color: props.color }}>
+			<p className="picker-color-value">{props.color}</p>
+			<p className={`picker-hold ${holdClass}`} onClick={() => props.toggleHold(props.id)}>
+				<i className={checkbox} />HOLD
+			</p>
+		</div>
+	);
 }
 
 export default PickerColor;
