a679efa6545ef54b9ef1adf30d0ef6556209a852
Lynne Rang <lynne.rang@gmail.com>
2019-07-06 10:25:22 -0600

    Add repurposing of menu to trash icon, since we don't need a rename option anymore.

--- src/Project/index.js
+++ src/Project/index.js
@@ -1,61 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Palette from '../Palette';
 import requests from '../utils/apiRequests';
 
-class Project extends Component {
-	constructor(props) {
-		super(props);
+function Project(props) {
+  const palettes = props.data.palettes.map(palette => <Palette data={palette} key={palette.id} />);
 
-		this.state = {
-			showMenu: false
-		};
-	}
-
-	toggleMenu = () => {
-		this.setState({ showMenu: !this.state.showMenu });
-	};
-
-  updateProjectName = e => {
-    requests.putProject({ project_name: e.target.textContent, id: this.props.data.id });
-	};
-
-	render() {
-		const palettes = this.props.data.palettes.map(palette => <Palette data={palette} key={palette.id} />);
-
-		return (
-			<article className="Project">
-				<div className="project-heading">
-					<div className="project-heading-left">
-						<i className="fas fa-folder-open" />
-						<h3
-              className="project-title"
-              contentEditable
-              suppressContentEditableWarning
-              onBlur={this.updateProjectName}
-              onKeyDown={e => {
-                if (e.keyCode === 13) {
-                  e.preventDefault();
-                  this.updateProjectName(e);
-                }
-              }}>
-							{this.props.data.name}
-						</h3>
-					</div>
-					<i className="fas fa-ellipsis-h" onClick={this.toggleMenu} />
-				</div>
-				<div className="project-palettes">
-					{palettes}
-					<NavLink exact to="/" className="Palette add-palette-btn">
-						<p className="plus-txt">+</p>
-						<p className="add-palette-txt">
-							Add New<br />Palette
-						</p>
-					</NavLink>
-				</div>
-			</article>
-		);
-	}
+  return (
+    <article className="Project">
+      <div className="project-heading">
+        <div className="project-heading-left">
+          <i className="fas fa-folder-open" />
+          <h3
+            className="project-title"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={e => requests.putProject({
+              project_name: e.target.textContent,
+              id: props.data.id
+            })}
+            onKeyDown={e => {
+              if (e.keyCode === 13) {
+                e.preventDefault();
+                requests.putProject({
+                  project_name: e.target.textContent,
+                  id: props.data.id
+                })
+              }
+            }}>
+            {props.data.name}
+          </h3>
+        </div>
+        <i className="fas fa-trash" onClick={() => requests.deleteProject({
+          id: props.data.id
+        })} />
+      </div>
+      <div className="project-palettes">
+        {palettes}
+        <NavLink exact to="/" className="Palette add-palette-btn">
+          <p className="plus-txt">+</p>
+          <p className="add-palette-txt">
+            Add New<br />Palette
+          </p>
+        </NavLink>
+      </div>
+    </article>
+  );
 }
 
 export default Project;
