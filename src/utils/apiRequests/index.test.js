68d5df999813c82227295f48489f681bfc34217a
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-06 10:30:09 -0600

    Setup testing suite for Requests

---
+++ src/utils/apiRequests/index.test.js
@@ -0,0 +1,7 @@
+import requests from './index'; 
+import { checkStatus, fetchAnything, deleteAnything, sendAnything } from './index';
+
+describe('Requests', () => {
+  it('should fetch anything', () => {
+  })
+})
