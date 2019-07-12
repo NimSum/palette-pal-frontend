452b1a1b049e5136e4ae1c318f8e616500bad354
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-11 22:33:42 -0600

    Updates

---
+++ precache-manifest.0eb57c2b41452308440f7dfea11322cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4dbf8daa7c3d3c90a35da832965eeb",
+    "url": "/palette-pal-frontend/index.html"
+  },
+  {
+    "revision": "08d15f0ed5176d59525b",
+    "url": "/palette-pal-frontend/static/css/main.61691ef3.chunk.css"
+  },
+  {
+    "revision": "1bb1bb60f056bdef7d38",
+    "url": "/palette-pal-frontend/static/js/2.cdae5da1.chunk.js"
+  },
+  {
+    "revision": "08d15f0ed5176d59525b",
+    "url": "/palette-pal-frontend/static/js/main.44fe203a.chunk.js"
+  },
+  {
+    "revision": "dfe1bdf92b601d3092f2",
+    "url": "/palette-pal-frontend/static/js/runtime~main.238f5916.js"
+  }
+]);
