40378f2eab72eb1e22b247411933fefa886c32e1
Nimrod Garcia <nimm.garcia@gmail.com>
2019-09-26 15:06:33 -0500

    Simplify two compononents into one

--- src/AllPalettesContainer/PublicPalette/index.js
+++ src/AllPalettesContainer/PublicPalette/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import PublicPaletteColor from './PublicPaletteColor';
+import colorFormatter from '../../utils/colorFormatter';
+import colorContraster from '../../utils/colorContraster';
 
-function Palette(props) {
+function PublicPalette(props) {
 	const paletteColors = [];
 
   for (let i = 1; i <= 5; i++) {
     const color = props.data[`color_${i}`];
-    
-    paletteColors.push(<PublicPaletteColor
-      color={color}
-      palette_id={props.data.id}
-      palette_name={props.data.name}
-      id={`color_${i}`}
-      key={`color_${i}`}
-      project_id={props.projectID}
-      updatePaletteData={props.updatePaletteData}
-      format={props.format}
-    />);
+    paletteColors.push(
+      <div
+        className="public-palette-color"
+        key={props.data.id}
+        style={{ backgroundColor: color }}
+      >
+        <p
+          className="color-hex"
+          style={{ color: colorContraster(color) }}
+        >
+          {colorFormatter(color, props.format)}
+        </p>
+      </div>);
 	}
 
 	return (
-		<div className="Palette">
+		<div className="PublicPalette">
 			{paletteColors}
 			<p
-				className="palette-title"
+				className="public-palette-title"
         >
 				{props.data.name}
 			</p>
