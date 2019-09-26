7e93c67079fe88fc713b69e81b4c9bba880b81a1
Nimrod Garcia <nimm.garcia@gmail.com>
2019-09-26 12:42:38 -0500

    Saves palettes using react hooks(state, effect)

--- src/AllPalettesContainer/AllPalettesContainer.js
+++ src/AllPalettesContainer/AllPalettesContainer.js
@@ -2,6 +2,16 @@ import React, { useState ,useEffect } from 'react'
 import requests from '../utils/apiRequests';
 
 export default function AllPalettesContainer() {
+  const [palettes, setPalettes] = useState([]);
+
+  const fetchPalettes = async () => {
+    const fetchedPaletttes = await requests.getPalettes();
+    setPalettes(fetchedPaletttes);
+  }
+
+  useEffect(() => {
+    fetchPalettes();
+  }, []);
 
   return (
     <div>
