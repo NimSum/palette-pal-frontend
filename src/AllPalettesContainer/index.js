bacf4bb2e1269bb349fa8bb3caf616948d6905a2
Nimrod Garcia <nimm.garcia@gmail.com>
2019-09-27 11:08:32 -0500

    Add subheader for other color options

--- src/AllPalettesContainer/index.js
+++ src/AllPalettesContainer/index.js
@@ -1,11 +1,18 @@
 import React, { useState ,useEffect } from 'react'
 import requests from '../utils/apiRequests';
 import PublicPalette from './PublicPalette/';
+import SubHeader from '../SubHeader';
+import Dialog from '../Dialog';
 
 export default function AllPalettesContainer() {
   const [palettes, setPalettes] = useState([]);
   const [hasErrored, setError] = useState(false);
   const [isLoading, setLoading] = useState(false);
+  const [isDialogVisible, showDialog] = useState(false);
+
+  const toggleDialog = (onOff) => {
+    showDialog(onOff);
+  };
 
   const fetchPalettes = async () => {
     setLoading(true);
@@ -33,9 +40,16 @@ export default function AllPalettesContainer() {
 
   return (
     <section className="AllPalettesContainer">
-      {paletteElements}
-      {hasErrored && <h2>Something went wrong :(</h2>}
-      {isLoading && <h3>Loading palettes...</h3>}
+      <SubHeader 
+        title="Pick New Palette"
+        btnTitle="Generate New Palette"
+        data={palettes}
+      />
+      <div className="palette-container-styling">
+        {paletteElements}
+        {hasErrored && <h2>Something went wrong :(</h2>}
+        {isLoading && <h3>Loading palettes...</h3>}
+      </div>
     </section>
   )
 }
